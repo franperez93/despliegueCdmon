7b3d29da57d76eb1db5314b06afdcd0282d6b133
Francisco Perez <frankiko.8@gmail.com>
2016-01-18 11:01:45 +0000

    segundo commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head> 
+<body>
+
+<h1>Si sale esto en produccion la he cagado </h1>
+
+<h3>Hola internauta, como estas</h3>
+
+<img src="randomicon.php"/>
+
+</body>
+</html>
+
+
+
